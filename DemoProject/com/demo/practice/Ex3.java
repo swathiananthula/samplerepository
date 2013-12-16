88d2f51a3919182f6a7a3f87eebfb18a9c42233a
swathiananthula <ananthulaswathi1991@gmail.com>
2013-12-16 17:05:09 +0530

    this is new project created and 1 file added to demoproject

---
+++ DemoProject/com/demo/practice/Ex3.java
@@ -0,0 +1,14 @@
+package com.demo.practice;
+
+public class Ex3 {
+	public void fun(){
+		System.out.println("am in fun");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+Ex3 e=new Ex3();
+e.fun();
+	}
+
+}
---
+++ demo/src/demo/Ex.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("dhfjahdfjk");
+	}
+
+}
